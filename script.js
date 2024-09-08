0f1548d4cd373b77b029d0b4d2f3b62fff4941c1
edoski <87473803+edoski@users.noreply.github.com>
2024-09-08 18:48:41 +0200

    tidied up

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ let mode = '';
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	
+	// Get the color and mode from the form, formatted for the API
 	const formData = new FormData(form);
 	color = formData.get('color').replace('#', '');
 	mode = formData.get('mode');
-
-	console.log(color, mode);
 	
+	// Fetch the color scheme from the API
 	fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=5`)
 		.then(response => response.json())
 		.then(data => {
+			// Reset the grid if previous colors are displayed
 			const grid = document.querySelector('.grid-colorscheme');
 			grid.innerHTML = "";
-			
+
+			// Display the colors in the grid
 			const colors = data.colors.map(color => color.hex.value);
 			colors.forEach(color => {
 				const column = document.createElement('div');
